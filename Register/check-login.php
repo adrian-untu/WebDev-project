e3b5fd69f1755e0386518a95563543d21621a9ba
adrian-untu <79197593+adrian-untu@users.noreply.github.com>
2022-04-07 13:33:10 +0300

    login

---
+++ Register/check-login.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html>
+<html>
+  
+<head>
+    <title>Insert Page page</title>
+</head>
+  
+<body>
+        <?php
+  
+        // servername => localhost
+        // username => root
+        // password => empty
+        // database name => staff
+        $conn = mysqli_connect("localhost", "root", "", "login");
+          
+        // Check connection
+        if($conn === false){
+            die("ERROR: Could not connect. " 
+                . mysqli_connect_error());
+        }
+          
+        // Taking all 5 values from the form data(input)
+        $nume =  $_POST['name'];
+        $parola = $_POST['password'];
+        // Performing insert query execution
+        // here our table name is login
+        $check_user = mysqli_query($conn, "SELECT name, password FROM login where name = '$nume' AND password = '$parola' ");
+if(mysqli_num_rows($check_user) > 0){
+    header('Location: http://localhost/PEM/admin.html');
+}
+else{
+    header('Location: http://localhost/PEM/Register/login.html');
+}
+        // Close connection
+        mysqli_close($conn);
+        ?>
+</body>
+  
+</html>

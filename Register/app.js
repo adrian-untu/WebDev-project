97dbb314758a109dac6522cd163319b87ed34356
DragosBadarau <74381554+DragosBadarau@users.noreply.github.com>
2022-04-13 18:36:54 +0300

    update login and register

--- Register/app.js
+++ Register/app.js
@@ -1,3 +1,8 @@
+//the animation
+//add a class to the container element when we click on sign up button on panel1
+//remove that class when we click on the sign in button from the panel2
+//switch from sign up form to the register and move the background
+
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");

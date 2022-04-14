1126a6673286346c085f361078d683f107f14607
DragosBadarau <74381554+DragosBadarau@users.noreply.github.com>
2022-04-14 20:43:30 +0300

    hide/show password

--- Register/app.js
+++ Register/app.js
@@ -14,3 +14,20 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+//show and hide the password
+const passwordEl = document.querySelector(".password");
+const eyeButton = document.querySelector(".fa");
+let isPass = true;
+function togglePass() {
+  if (isPass) {
+    passwordEl.setAttribute("type", "text");
+    eyeButton.classList.remove("fa-eye-slash");
+    eyeButton.classList.add("fa-eye");
+    isPass = false;
+  } else {
+    passwordEl.setAttribute("type", "password");
+    eyeButton.classList.remove("fa-eye");
+    eyeButton.classList.add("fa-eye-slash");
+    isPass = true;
+  }
+}

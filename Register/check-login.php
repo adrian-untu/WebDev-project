9fc4f78b400e3c3e82d7bf3cecbb0dac755afdc3
adrian-untu <79197593+adrian-untu@users.noreply.github.com>
2022-04-15 00:22:57 +0300

    all users linked to their respective pages

--- Register/check-login.php
+++ Register/check-login.php
@@ -23,7 +23,18 @@
         $parola = $_POST['password'];
         $check_user = mysqli_query($conn, "SELECT name, password FROM login where name = '$nume' AND password = '$parola' ");
 if(mysqli_num_rows($check_user) > 0){
-    header('Location: http://localhost/PEM/admin.html');
+    switch($nume){
+    case 'admin':
+        header('Location: http://localhost/PEM/admin.html');
+        break;
+    case 'Dragos':
+        header('Location: http://localhost/PEM/user1.html');
+        break;
+    case 'Adrian':
+        header('Location: http://localhost/PEM/user2.html');
+        break;
+    default:
+    }
 }
 else{
     header('Location: http://localhost/PEM/Register/login.html');

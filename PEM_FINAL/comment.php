f4a37b24d7fe1f657f2b49de32854fc2517e1ed1
adrian-untu <79197593+adrian-untu@users.noreply.github.com>
2022-05-26 00:45:25 +0300

    comments now work properly

--- PEM_FINAL/comment.php
+++ PEM_FINAL/comment.php
@@ -4,18 +4,18 @@
 	
 	if (isset($_POST['post_comment']))
 	{
-		$user=$_SESSION['user_id'];
+		$user=$_SESSION['id'];
 		$content_comment=$_POST['content_comment'];
 		$post_id=$_POST['post_id'];
-		$user=$_POST['user'];
+		$user_id=$_POST['user_id'];
 		$time=time();
 		
 
 		{
-            $sql1=mySQLi_query($con,"select * from comments");
+			$sql1=mySQLi_query($con,"select * from comments");
 			$count = mySQLi_num_rows($sql1) + 1;
 			mySQLi_query($con,"INSERT INTO comments (comment_id,post_id,user_id,name,content_comment,image,created)
-			VALUES ('$count','$post_id','$id','$user','$content_comment','$profile_picture',$time)");
+			VALUES ('$count','$post_id','$id','$user_id','$content_comment','$profile_picture',$time)");
 			echo "<script>window.location='home.php'</script>";
 		}
 			
--- PEM_FINAL/home.php
+++ PEM_FINAL/home.php
@@ -174,24 +174,25 @@ if($years==1)
 
 <?php
 	include("includes/database.php");
-			$comment=mySQLi_query($con,"SELECT * from comments where post_id='$post_id' order by post_id DESC");
-			while($row=mySQLi_fetch_array($comment)){
-			$comment_id=$row['comment_id'];
-			$content_comment=$row['content_comment'];
-			$time=$row['created'];	
-			$post_id=$row['post_id'];
-			$user=$_SESSION['user_id'];
-			
+	include("includes/database.php");
+	$comment=mySQLi_query($con,"SELECT * from comments where post_id='$post_id' order by post_id DESC");
+	while($row=mySQLi_fetch_array($comment)){
+	$comment_id=$row['comment_id'];
+	$content_comment=$row['content_comment'];
+		$time=$row['created'];	
+	$post_id=$row['post_id'];
+	$user=$_SESSION['id'];
+	
 ?>			
-			<div class="comment-display"<?php echo $comment_id ?>>
-					<div class="delete-post">
-					<a href="delete_comment.php<?php echo '?id='.$comment_id; ?>" title="Delete your comment"><button class="btn-delete">X</button></a>
-					</div>
-				<div class="user-comment-name"><img src="<?php echo $row['image']; ?>">&nbsp;&nbsp;&nbsp;<?php echo $row['name']; ?><b class="time-comment"><?php echo $time ?></b></div>
-				<div class="comment"><?php echo $row['content_comment']; ?></div>
-			
+	<div class="comment-display"<?php echo $comment_id ?>>
+			<div class="delete-post">
+			<a href="delete_comment.php<?php echo '?id='.$comment_id; ?>" title="Delete your comment"><button class="btn-delete">Delete comment</button></a>
 			</div>
-			<br />
+		<div class="user-comment-name"><img src="<?php echo $row['image']; ?>">&nbsp;&nbsp;&nbsp;<?php echo $row['name']; ?><b class="time-comment"><?php echo $time = time_stamp($time); ?></b></div>
+		<div class="comment"><?php echo $row['content_comment']; ?></div>
+	
+	</div>
+	<br />
 
 <?php
 }
@@ -209,10 +210,10 @@ if($years==1)
 						<img src="<?php echo $row['profile_picture']; ?>" width="50" height="50">
 						<?php } ?>
 			
-			<input type="text" name="content_comment" placeholder="Write a comment..." class="comment-text">
-			<input type="hidden" name="post_id" value="<?php echo $row['post_id'] ?>">
-			<input type="hidden" name="user" value="<?php echo $row['username'] ?>">
-			<input type="hidden" name="image" value="<?php echo $row['profile_picture']  ?>">
+						<input type="text" name="content_comment" placeholder="Write a comment..." class="comment-text">
+			<input type="hidden" name="post_id" value="<?php echo $post_id ?>">
+			<input type="hidden" name="user_id" value="<?php echo $firstname . ' ' . $lastname  ?>">
+			<input type="hidden" name="image" value="<?php echo $profile_picture  ?>">
 			<input type="submit" name="post_comment" value="Enter" class="btn-comment">
 			
 			</div>

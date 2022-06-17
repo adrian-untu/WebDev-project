0b8e00faa94cbfc659c90d51d116932d1c832421
DragosBadarau <74381554+DragosBadarau@users.noreply.github.com>
2022-06-17 21:38:09 +0300

    logical signup updates
    for security

--- PEM_FINAL/PEM_FINAL.sql
+++ PEM_FINAL/PEM_FINAL.sql
@@ -129,7 +129,6 @@ CREATE TABLE `user` (
   `birthday` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `password2` varchar(100) NOT NULL,
   `profile_picture` varchar(100) DEFAULT NULL,
   `family_id` int(100)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -138,14 +137,14 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `firstname`, `lastname`, `username`, `birthday`, `email`, `password`, `password2`, `profile_picture`, `family_id`) VALUES
-(1, 'Adrian', 'Untu', 'adrian_untu', '04/21/2001', 'adrianuntu2001@gmail.com', '12345', '12345', 'upload/Adrian_profile.jpg', 1),
-(2, 'Dragos', 'Badarau', 'dragos_badarau', '08/07/2001', 'dragosb2001@gmail.com', '12345', '12345', 'upload/Dragos_profile.jpg', 2),
-(3, 'George', 'Clooney', 'george_clooney', '1996-04-12', 'smth@clooney.com', '12345', '12345', 'upload/default_profile.jpg', 3),
-(4, 'Dana', 'Untu', 'dana_untu', '1975-03-15', 'dana@gmail.com', '12345', '12345', 'upload/default_profile.jpg', 1),
-(5, 'Ana', 'Prodan', 'ana_prodan', '2001-07-22', 'ana@gmail.com', '12345', '12345', 'upload/default_profile.jpg', 2),
-(6, 'Barrack', 'Obama', 'barrack_obama', '1976-04-18', 'barrack@gmail.com', '12345', '12345', 'upload/default_profile.jpg', 4),
-(7, 'Carmen', 'Botosineanu', 'carmen_botosineanu', '2001-04-09', 'carmen@gmail.com', '12345', '12345', 'upload/default_profile.jpg', 1);
+INSERT INTO `user` (`user_id`, `firstname`, `lastname`, `username`, `birthday`, `email`, `password`,  `profile_picture`, `family_id`) VALUES
+(1, 'Adrian', 'Untu', 'adrian_untu', '04/21/2001', 'adrianuntu2001@gmail.com', '12345',  'upload/Adrian_profile.jpg', 1),
+(2, 'Dragos', 'Badarau', 'dragos_badarau', '08/07/2001', 'dragosb2001@gmail.com', '12345',  'upload/Dragos_profile.jpg', 2),
+(3, 'George', 'Clooney', 'george_clooney', '1996-04-12', 'smth@clooney.com', '12345', 'upload/default_profile.jpg', 3),
+(4, 'Dana', 'Untu', 'dana_untu', '1975-03-15', 'dana@gmail.com', '12345', 'upload/default_profile.jpg', 1),
+(5, 'Ana', 'Prodan', 'ana_prodan', '2001-07-22', 'ana@gmail.com', '12345',  'upload/default_profile.jpg', 2),
+(6, 'Barrack', 'Obama', 'barrack_obama', '1976-04-18', 'barrack@gmail.com', '12345',  'upload/default_profile.jpg', 4),
+(7, 'Carmen', 'Botosineanu', 'carmen_botosineanu', '2001-04-09', 'carmen@gmail.com', '12345',  'upload/default_profile.jpg', 1);
 
 -- --------------------------------------------------------
 
--- PEM_FINAL/signup.php
+++ PEM_FINAL/signup.php
@@ -5,32 +5,56 @@
 	if (isset($_POST['submit']))
 	{
 		$username=$_POST['username'];
+		$count=count(explode('_',$username));
+		if($count!=2)
+		{
+			echo "<script>alert('Please introduce your username with the following format: firstname_lastname!'); window.location='login_register.php'</script>";
+			exit();
+		}
+
+		$firstname=ucfirst(explode('_',$username)[0]); 
+		$lastname=ucfirst(explode('_',$username)[1]); 
+
+		if(strlen($firstname)<2 || strlen($lastname)<2)
+		{
+				echo "<script>alert('Your firstname or lastname is invalid! '); window.location='login_register.php'</script>";
+				exit();
+		}
+
 		$birthday=$_POST['birthday'];
-		$firstname=ucfirst(explode('_',$username)[0]);
-		$lastname=ucfirst(explode('_',$username)[1]);
 		$email=$_POST['email'];
 		$password=$_POST['password'];
 		$password2=$_POST['password2'];
 		
-			$sql=mySQLi_query($con,"select * from user WHERE email='$email'");
-			$row=mySQLi_num_rows($sql);
-			if ($row > 0)
+		$sql=mySQLi_query($con,"select * from user WHERE email='$email'");
+		$row=mySQLi_num_rows($sql);
+		if ($row > 0)
 			{
-			echo "<script>alert('E-mail already taken!'); window.location='login_register.php'</script>";
+				echo "<script>alert('E-mail already taken!'); window.location='login_register.php'</script>";
+				exit();
 			}
-			elseif($password != $password2)
+			
+		$sql=mySQLi_query($con,"select * from user WHERE username='$username'");
+		$row=mySQLi_num_rows($sql);
+		if ($row > 0)
+			{
+				echo "<script>alert('Username already taken!'); window.location='login_register.php'</script>";
+				exit();
+			}
+			
+		if($password != $password2)
 			{
 			echo "<script>alert('Password do not match!'); window.location='login_register.php'</script>";
+			exit();
 			}
-            else
+        else
 			{
 				$sql1=mySQLi_query($con,"select * from user");
 				$count = mySQLi_num_rows($sql1) + 1;
-				mySQLi_query($con,"INSERT INTO user (user_id,firstname,lastname,username,birthday,email,password,password2,profile_picture)
-				VALUES ('$count','$firstname','$lastname','$username','$birthday','$email','$password','$password2', 'upload/default_profile.jpg')");
+				mySQLi_query($con,"INSERT INTO user (user_id,firstname,lastname,username,birthday,email,password,profile_picture)
+				VALUES ('$count','$firstname','$lastname','$username','$birthday','$email','$password', 'upload/default_profile.jpg')");
 				echo "<script>alert('Account successfully created!'); window.location='login_register.php'</script>";
 			}
-			
 	}
 	
 ?>

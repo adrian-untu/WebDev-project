ea58c4c93fd6cc7f95c147c2ecf4d07f1824d3f2
adrian-untu <79197593+adrian-untu@users.noreply.github.com>
2022-06-21 10:52:31 +0300

    Update view_medical_history_script.php

--- PEM_LAST/app/scripts/view_medical_history_script.php
+++ PEM_LAST/app/scripts/view_medical_history_script.php
@@ -1,12 +1,5 @@
 <?php
-if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')   
-             $url = "https://";   
-        else  
-             $url = "http://";   
-        $url.= $_SERVER['HTTP_HOST'];   
-        $url.= $_SERVER['REQUEST_URI'];    
-        if(preg_match_all('/\d+/', $url, $numbers))
-        $id_pet = end($numbers[0]);
+$id_pet = htmlspecialchars($_GET['pet']);
 $stmt = $pdo->prepare("SELECT * FROM medical_history LEFT JOIN pets on medical_history.pet_id = pets.pet_id WHERE pets.pet_id = ? ORDER BY medical_history.date DESC");
 $stmt->execute(["$id_pet"]);
 $results = $stmt->fetchAll();
